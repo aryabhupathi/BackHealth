302068d9ed05ec955ba984ab6969718a860ded28
Arya Bhupathi <bhupathikonduru@gmail.com>
2025-12-02 22:10:23 +0530

    backdeploy1

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import express from "express";
 dotenv.config();
 const app = express();
 app.use(express.json());
-const allowedOrigin = process.env.FRONTEND_ORIGIN || "https://backhealth.onrender.com";
+const allowedOrigin = process.env.FRONTEND_ORIGIN || "https://front-health.vercel.app";
 app.use(
 cors({
   origin: allowedOrigin,
